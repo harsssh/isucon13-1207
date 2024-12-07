990f772366eaa57f82c59c29d9a411f5ae404da7
tkuramot <mabdman512@gmail.com>
2024-12-07 17:03:48 +0900

    feat(database): add index to livestream_tags for optimization

--- app/sql/migration.sql
+++ app/sql/migration.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `ng_words` ADD INDEX `livestream_id_idx`(livestream_id);
+ALTER TABLE `livestream_tags` ADD INDEX `tag_ids_livestream_id`(tag_id, livestream_id DESC);
 ALTER TABLE `livestream_tags` ADD INDEX `livestream_id_idx`(livestream_id);
