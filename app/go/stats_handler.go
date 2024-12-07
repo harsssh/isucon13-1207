3ba0171492c8490ede306426a1e155dd02b58177
smatsuodev <git@smatsuo.dev>
2024-12-07 18:43:27 +0900

    fix N+1 in getLivestreamStatisticsHandler

--- app/go/stats_handler.go
+++ app/go/stats_handler.go
@@ -228,24 +228,36 @@ func getLivestreamStatisticsHandler(c echo.Context) error {
 	}
 
 	var livestreams []*LivestreamModel
-	if err := tx.SelectContext(ctx, &livestreams, "SELECT * FROM livestreams"); err != nil && !errors.Is(err, sql.ErrNoRows) {
+	if err := tx.SelectContext(ctx, &livestreams, "SELECT * FROM livestreams l ORDER BY l.id"); err != nil && !errors.Is(err, sql.ErrNoRows) {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get livestreams: "+err.Error())
+	}
+
+	var reactions []int64
+	if err := tx.SelectContext(ctx, &reactions, `
+			SELECT COUNT(r.id)
+			FROM livestreams l
+					 LEFT JOIN reactions r ON l.id = r.livestream_id
+			GROUP BY l.id
+			ORDER BY l.id DESC
+	`); err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to count reactions: "+err.Error())
+	}
+
+	var totalTips []int64
+	if err := tx.SelectContext(ctx, &totalTips, `
+			SELECT IFNULL(SUM(l2.tip), 0)
+			FROM livestreams l
+					 LEFT JOIN livecomments l2 ON l.id = l2.livestream_id
+			GROUP BY l.id
+			ORDER BY l.id DESC;
+	`, livestream.ID); err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to count tips: "+err.Error())
 	}
 
 	// ランク算出
 	var ranking LivestreamRanking
-	for _, livestream := range livestreams {
-		var reactions int64
-		if err := tx.GetContext(ctx, &reactions, "SELECT COUNT(*) FROM livestreams l INNER JOIN reactions r ON l.id = r.livestream_id WHERE l.id = ?", livestream.ID); err != nil && !errors.Is(err, sql.ErrNoRows) {
-			return echo.NewHTTPError(http.StatusInternalServerError, "failed to count reactions: "+err.Error())
-		}
-
-		var totalTips int64
-		if err := tx.GetContext(ctx, &totalTips, "SELECT IFNULL(SUM(l2.tip), 0) FROM livestreams l INNER JOIN livecomments l2 ON l.id = l2.livestream_id WHERE l.id = ?", livestream.ID); err != nil && !errors.Is(err, sql.ErrNoRows) {
-			return echo.NewHTTPError(http.StatusInternalServerError, "failed to count tips: "+err.Error())
-		}
-
-		score := reactions + totalTips
+	for i, livestream := range livestreams {
+		score := reactions[i] + totalTips[i]
 		ranking = append(ranking, LivestreamRankingEntry{
 			LivestreamID: livestream.ID,
 			Score:        score,
